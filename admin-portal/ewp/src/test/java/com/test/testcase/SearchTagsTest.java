1cd942d98f74e36a0b5c70a5c22349a945b54664
Eric <eric.lu@sinodynamic.com>
2019-03-14 11:50:18 +0800

    update test code

--- admin-portal/ewp/src/test/java/com/test/testcase/SearchTagsTest.java
+++ admin-portal/ewp/src/test/java/com/test/testcase/SearchTagsTest.java
@@ -65,6 +65,12 @@ public class SearchTagsTest extends AbstractTestNGSpringContextTests {
         return (Iterator<Object[]>)new CsvUtils("participantData.csv",path);
     }
 
+    @DataProvider(name="fullCompanyInformationData")
+    public Iterator<Object[]> fullCompanyInformationDto() throws IOException {
+        String path=".src.main.java.com.gea.portal.ewp.testData."+currentEnvironment+"TestData.";
+        return (Iterator<Object[]>)new CsvUtils("fullCompanyInformationData.csv",path);
+    }
+
     @Test(dataProvider="pageData")
     public void testListParticipant(Map<String, String> data) throws IOException {
         String token=data.get("token");
@@ -86,6 +92,80 @@ public class SearchTagsTest extends AbstractTestNGSpringContextTests {
 //        再Json化成对象
         Assert.assertNotNull(restResponse, "response");
     }
+
+    @Test(dataProvider="fullCompanyInformationData")
+    public void testCreateFullCompanyInformation(Map<String, String> data) throws IOException {
+        String token=data.get("token");
+        UserDetails userDetails = userService.getUserDetailByToken(token);
+        if(null!=userDetails){
+            AnaPrincipalAuthenticationToken authentication = new AnaPrincipalAuthenticationToken(userDetails,token,"");
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        FullCompanyInformationDto postDto=new FullCompanyInformationDto();
+        postDto=getPostDtoData(postDto,data);
+        RestfulResponse<String> restResponse=ewalletParticipantService.createFullCompanyInformation(postDto);
+        Assert.assertNotNull(restResponse, "response");
+//        再Json化成对象
+        Assert.assertNotNull(restResponse, "response");
+    }
+
+    private FullCompanyInformationDto getPostDtoData(FullCompanyInformationDto postDto, Map<String, String> data) {
+        postDto.setParticipantId(data.get("participantId"));
+        postDto.setRegistrationDate(data.get("registrationDate"));
+        postDto.setAddress(data.get("address"));
+        postDto.setCountry(data.get("country"));
+        postDto.setFullCompanyName(data.get("fullCompanyName"));
+        postDto.setNotificationEmail(data.get("notificationEmail"));
+        postDto.setParticipantName(data.get("participantName"));
+        postDto.setRequestRemark(data.get("requestRemark"));
+        postDto.setRegistrationNo(data.get("registrationNo"));
+        //
+        List<DisputeContactDto> disputeContactDtoList=new ArrayList<>();
+        DisputeContactDto disputeContactDto=new DisputeContactDto();
+        disputeContactDto.setRoleName("testnum5");
+        disputeContactDto.setPhoneOffice("13214236554");
+        disputeContactDto.setPhoneMobile("13214236554");
+        disputeContactDto.setNameEn("testnum5");
+        disputeContactDto.setMobileSms(true);
+        disputeContactDto.setEmail("testnum5@qq.com");
+        disputeContactDto.setDepartmentName("testnum5");
+        disputeContactDtoList.add(disputeContactDto);
+        postDto.setDisputeContactDto(disputeContactDtoList);
+        //
+        KeyPersonInformationDto keyPersonInformationDto=new KeyPersonInformationDto();
+        keyPersonInformationDto.setDepartment("testnum5");
+        keyPersonInformationDto.setDirectLine("13214236554");
+        keyPersonInformationDto.setEmail("testnum5@qq.com");
+        keyPersonInformationDto.setMobileNumber("13214236554");
+        keyPersonInformationDto.setName("testnum5");
+        keyPersonInformationDto.setReceiveSms(true);
+        keyPersonInformationDto.setRole("testnum5");
+        List<KeyPersonInformationDto> keyPersonInformationDtoList=new ArrayList<>();
+        keyPersonInformationDtoList.add(keyPersonInformationDto);
+        postDto.setKeyPersonInformationDto(keyPersonInformationDtoList);
+        //
+        MaterialDto materialDto=new MaterialDto();
+        materialDto.setFilePath("46160833_e5a7_4b58_8018_3224075f7fcf.png");
+        materialDto.setMaterialFilename("u=415293130 - 副本.png");
+        postDto.setMaterialDto(materialDto);
+        //
+        OwnerDetailsDto ownerDetailsDto=new OwnerDetailsDto();
+        ownerDetailsDto.setDirectLine("13214236554");
+        ownerDetailsDto.setEmail("testnum5@qq.com");
+        ownerDetailsDto.setMobileNumber("13214236554");
+        MaterialDto materialDto1=new MaterialDto();
+        materialDto1.setFilePath("464fcc91_7244_4cf5_9171_070f6068fea7.png");
+        materialDto1.setMaterialFilename("u=415293130 - 副本.png");
+        ownerDetailsDto.setMaterialDto(materialDto1);
+        ownerDetailsDto.setName("testnum5");
+        ownerDetailsDto.setRole("testnum5");
+        List<OwnerDetailsDto> ownerDetailsDtoList=new ArrayList<>();
+        ownerDetailsDtoList.add(ownerDetailsDto);
+        postDto.setOwnerDetailsDto(ownerDetailsDtoList);
+        return postDto;
+    }
+
 
     @Test(dataProvider="pageData")
     public void testGetServiceAssignment(Map<String, String> data) throws IOException {
